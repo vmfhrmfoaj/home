ea405cd17ea9b655a105c9a1481cca58d5b5a9a0
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-04-02 16:03:58 +0900

    Bump the version 'cider-nrepl' and 'refactor-nrepl'

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                       [refactor-nrepl    "2.2.0-SNAPSHOT"]]
+{:user {:plugins      [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
+                       [refactor-nrepl    "2.3.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
