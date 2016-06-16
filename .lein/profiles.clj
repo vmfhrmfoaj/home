048eeb696c773ab45cd5b4e780d97b700ae2c869
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-06-16 14:40:40 +0900

    Bump the version of Clojure plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,6 @@
-{:user {:plugins      [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
+{:user {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                       [org.clojure/tools.nrepl "0.2.12"]]
+        :plugins      [[lein-pprint "1.1.2"]
+                       [cider/cider-nrepl "0.13.0-SNAPSHOT"]
                        [refactor-nrepl    "2.3.0-SNAPSHOT"]]
-        :dependencies [[com.cemerick/piggieback "0.2.1"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
+        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
