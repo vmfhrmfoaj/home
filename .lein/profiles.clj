c51dc728931b00c1a60f1ec2d8d98a57e41dad6b
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-07-24 19:45:54 +0900

    Add the plugin for Clojure

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,5 +3,6 @@
                        [figwheel-sidecar "0.5.3-2"]]
         :plugins      [[lein-pprint "1.1.2"]
                        [cider/cider-nrepl "0.13.0-SNAPSHOT"]
-                       [refactor-nrepl "2.3.0-SNAPSHOT"]]
+                       [refactor-nrepl "2.3.0-SNAPSHOT"]
+                       [lein-cljfmt "0.5.3"]]
         :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
