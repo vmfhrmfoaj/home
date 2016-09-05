8fd52482b898c69418a4213a4a978449268e406f
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-09-05 21:49:13 +0900

    Remove setting up for CLJS REPL

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,5 @@
-{:user {:dependencies [[com.cemerick/piggieback "0.2.1"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       [figwheel-sidecar "0.5.3-2"]]
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
         :plugins      [[lein-pprint "1.1.2"]
                        [cider/cider-nrepl "0.14.0-SNAPSHOT"]
                        [refactor-nrepl "2.3.0-SNAPSHOT"]
-                       [lein-cljfmt "0.5.3"]]
-        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+                       [lein-cljfmt "0.5.3"]]}}
