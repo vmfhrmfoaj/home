4dc2e1f9735a60019ba53b4ca70f45485879cbd8
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-10-18 21:53:11 +0900

    Update .profile.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
         :plugins      [[lein-pprint "1.1.2"]
-                       [cider/cider-nrepl "0.14.0-SNAPSHOT"]
+                       [cider/cider-nrepl "0.15.0-SNAPSHOT"]
                        [refactor-nrepl "2.3.0-SNAPSHOT"]
                        [lein-cljfmt "0.5.3"]]}}
