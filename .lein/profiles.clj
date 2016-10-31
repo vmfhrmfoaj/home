e1afe4f2c8db960b55c61847256239a056fd0e53
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-10-31 12:34:34 +0900

    Update .profiles.clj; include the figwheel

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,8 @@
-{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-        :plugins      [[lein-pprint "1.1.2"]
-                       [cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                       [refactor-nrepl "2.3.0-SNAPSHOT"]
-                       [lein-cljfmt "0.5.3"]]}}
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [com.cemerick/piggieback "0.2.1"]
+                       [figwheel-sidecar "0.5.8"]]
+        :plugins [[lein-pprint "1.1.2"]
+                  [cider/cider-nrepl "0.15.0-SNAPSHOT"]
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]
+                  [lein-cljfmt "0.5.3"]]
+        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
