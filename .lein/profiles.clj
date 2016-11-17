4331acdf90603a0a60947d0135b6cb88e289af8b
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2016-11-17 15:59:23 +0900

    Update .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,11 @@
-{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [com.cemerick/piggieback "0.2.1"]
-                       [figwheel-sidecar "0.5.8"]]
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
         :plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
+                  [com.jakemccrary/lein-test-refresh "0.17.0"]
+                  [lein-cider-figwheel-helper "0.0.1"]
                   [lein-cljfmt "0.5.3"]
+                  [lein-expectations-plus "0.0.2"]
                   [lein-pprint "1.1.2"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]
         :repl-options {:init (do
                                (set! *print-level* 8)
-                               (set! *print-length* 30))
-                       :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+                               (set! *print-length* 30))}}}
