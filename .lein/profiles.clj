67291a14b7c0f6693085b3e575eb25c245cd5d30
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2017-04-01 12:17:39 +0900

    Update .lein/profile.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
-{:user {:plugins [[lein-cljfmt "0.5.3"]
-                  [lein-dev-helper "0.0.2-SNAPSHOT"]
+{:user {:plugins [[lein-create-template "0.2.0"]
+                  [lein-cljfmt "0.5.3"]
+                  [lein-dev-helper "0.0.3-SNAPSHOT"]
                   [lein-expectations-plus "0.0.4-SNAPSHOT"]
                   [lein-pprint "1.1.2"]]
         :repl-options {:init (do
