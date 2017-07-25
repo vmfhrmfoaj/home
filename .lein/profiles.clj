939ae89dfeb8a361402eedd133cfe7107edb7d03
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2017-07-25 10:33:55 +0900

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,8 +2,9 @@
                   [lein-cljfmt "0.5.3"]
                   [lein-count  "1.0.7"]
                   [lein-pprint "1.1.2"]
-                  [lein-dev-helper  "0.0.3-SNAPSHOT"]
-                  [lein-test-helper "0.0.4-SNAPSHOT"]]
+                  [lein-project-version "0.1.0"]
+                  [lein-dev-helper  "0.0.3"]
+                  [lein-test-helper "0.0.4"]]
         :repl-options {:init (do
                                (set! *print-level* 8)
                                (set! *print-length* 30))}}}
