c90009c654c1cba5c6ce60d770532baa57fe5efb
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2017-09-24 00:25:35 +0900

    Update .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,7 @@
                   [lein-count  "1.0.7"]
                   [lein-pprint "1.1.2"]
                   [lein-project-version "0.1.0"]
-                  [lein-dev-helper  "0.1.0"]
+                  [lein-dev-helper  "0.1.1"]
                   [lein-test-helper "0.1.2"]]
         :repl-options {:init (do
                                (set! *print-level* 8)
