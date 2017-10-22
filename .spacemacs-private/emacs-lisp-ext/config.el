ec853022e3b1407b0a80ce1ce14b0c0af5633478
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2017-10-22 12:02:26 +0900

    feat(emacs/lisp-lang): updated to highlight local variables

---
+++ .spacemacs-private/emacs-lisp-ext/config.el
@@ -0,0 +1,3 @@
+(defface lisp-local-binding-variable-name-face
+  '((t (:inherit font-lock-variable-name-face :weight normal)))
+  "Face used to font-lock Lisp local binding variable name.")
--- .spacemacs-private/emacs-lisp-ext/packages.el
+++ .spacemacs-private/emacs-lisp-ext/packages.el
@@ -17,7 +17,7 @@
 (defun emacs-lisp-ext/post-init-emacs-lisp ()
   (font-lock-add-keywords
    'emacs-lisp-mode
-   '(("\\s(\\(\\(?:-as\\|-some\\)?->>?\\|and\\|or\\)\\_>"
+   `(("\\s(\\(\\(?:-as\\|-some\\)?->>?\\|and\\|or\\)\\_>"
       1 'default nil)
      ("(\\(lexical-let\\*?\\)"
       1 'font-lock-keyword-face)
@@ -29,11 +29,37 @@
       1 'font-lock-string-face)
      ;; spacemacs style function
      ("(defun[ \r\t\n]+\\([-+/=>&?0-9a-zA-Z]+\\)"
-      1 'font-lock-function-name-face t)))
+      1 'font-lock-function-name-face t)
+     ;; local variables
+     ("(\\(?:lexical\\)?let\\*? ("
+      (,(byte-compile
+         (lambda (limit)
+           (ignore-errors
+             (when font-lock--skip
+               (error ""))
+             (comment-forward (point-max))
+             (let ((local-limit (save-excursion (forward-sexp) (point))))
+               (unless (and (re-search-forward "(\\([-+/=>&?0-9a-zA-Z]+\\)[ \r\t\n]+"
+                                               (min local-limit limit) t))
+                 (set-match-data (-repeat 4 (point-min-marker))))
+               (goto-char local-limit))
+             t)))
+       (save-excursion
+         (if (in-comment?)
+             (setq-local font-lock--skip t)
+           (setq-local font-lock--skip nil)
+           (setq-local lisp--binding-form-point (point))
+           (safe-up-list-1)
+           (point)))
+       (if font-lock--skip
+           (end-of-line)
+         (goto-char lisp--binding-form-point))
+       (1 'lisp-local-binding-variable-name-face)))))
   (add-hook 'lisp-interaction-mode-hook #'smartparens-mode)
   (add-hook 'emacs-lisp-mode-hook
             (lambda ()
-              (setq-local auto-indent-block-level 3)))
+              (setq-local auto-indent-block-level 3)
+              (setq-local font-lock-multiline t)))
   (with-eval-after-load 'evil
     (let ((f (byte-compile
               (lambda ()
