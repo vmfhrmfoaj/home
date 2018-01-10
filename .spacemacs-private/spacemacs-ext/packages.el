89c79646f97870c64ca9e9043baef8ef8a4d236c
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-01-10 22:27:05 +0900

    chore(emacs): clear search highlights when starting typing

--- .spacemacs-private/spacemacs-ext/packages.el
+++ .spacemacs-private/spacemacs-ext/packages.el
@@ -78,7 +78,10 @@
   (when (require 'evil nil 'noerr)
     (define-key evil-ex-map (kbd "C-h") #'delete-backward-char)
     (define-key evil-insert-state-map (kbd "C-h") #'delete-backward-char)
-    (add-hook 'evil-normal-state-entry-hook #'auto-indent)
+    (add-hook 'evil-normal-state-entry-hook
+              (lambda ()
+                (auto-indent)
+                (evil-ex-nohighlight)))
     (advice-add #'open-line :after #'auto-indent)
     (dolist (fn '(evil-change evil-delete evil-join evil-paste-after))
       (advice-add fn :after #'auto-indent))
