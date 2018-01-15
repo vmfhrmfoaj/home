6256a6ea59df07a95821b4a1977070f2185a1f45
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-01-15 22:13:57 +0900

    fix(emacs): add a hook 'evil-normal-state-entry-hook' to align indentation automatically
    See, 7501ea commit.

--- .spacemacs-private/spacemacs-ext/packages.el
+++ .spacemacs-private/spacemacs-ext/packages.el
@@ -78,10 +78,8 @@
   (when (require 'evil nil 'noerr)
     (define-key evil-ex-map (kbd "C-h") #'delete-backward-char)
     (define-key evil-insert-state-map (kbd "C-h") #'delete-backward-char)
-    (add-hook 'evil-insert-state-entry-hook
-              (lambda ()
-                (auto-indent)
-                (evil-ex-nohighlight)))
+    (add-hook 'evil-normal-state-entry-hook #'auto-indent)
+    (add-hook 'evil-insert-state-entry-hook #'evil-ex-nohighlight)
     (advice-add #'open-line :after #'auto-indent)
     (dolist (fn '(evil-change evil-delete evil-join evil-paste-after))
       (advice-add fn :after #'auto-indent))
