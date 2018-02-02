18f7adfca65d8fac1685d7389b0669f150e86781
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-02-03 00:25:19 +0900

    feat(emacs): override `helm-swoop--get-content` to improve compability with `fancy-narrow`

--- .spacemacs-private/eye-candy/packages.el
+++ .spacemacs-private/eye-candy/packages.el
@@ -243,15 +243,14 @@
     :config
     (advice-add #'fancy-narrow-to-region :after
                 (lambda (&rest _)
-                  (setq helm-swoop-list-cache nil)))
+                  (setq-local helm-swoop-list-cache nil)))
     (advice-add #'fancy-widen :after
                 (lambda (&rest _)
-                  (setq helm-swoop-list-cache nil)))
-    ;; NOTE:
-    ;;  forcedly reload to load non-compiled `helm-swoop'.
-    ;;  it can cuase performance problems.
-    ;;  see http://nullprogram.com/blog/2013/01/22/
-    (load "helm-swoop.el" nil nil t)))
+                  (kill-local-variable 'helm-swoop-list-cache)))
+    (require 'dash-functional)
+    (byte-compile #'custom-helm-swoop--get-content)
+    (with-eval-after-load "helm-swoop"
+      (advice-add #'helm-swoop--get-content :override #'custom-helm-swoop--get-content))))
 
 (defun eye-candy/post-init-golden-ratio ()
   (use-package golden-ratio
