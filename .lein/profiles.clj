55672246f250950da7d57278897a3c6314e00f3f
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-04-01 10:40:30 +0900

    fix(profile): bump up the version of 'lein-dev-helper'

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,7 @@
                   [lein-count  "1.0.7"]
                   [lein-pprint "1.1.2"]
                   [lein-project-version "0.1.0"]
-                  [lein-dev-helper  "0.1.4-SNAPSHOT"]
+                  [lein-dev-helper  "0.1.4"]
                   [lein-test-helper "0.1.3-SNAPSHOT"]]
         :repl-options {:init (do
                                (set! *print-level* 8)
