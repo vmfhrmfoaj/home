24c6a3faa3b4b53e0b6b1cf7fb01c7f770461789
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-05-24 20:44:55 +0900

    fix(emacs): update the evil-surround settings for HHKB

--- .emacs.d/config/500-edit.el
+++ .emacs.d/config/500-edit.el
@@ -1,12 +1,35 @@
 (use-package aggressive-indent
   :ensure t
   :config
+  (add-to-list 'aggressive-indent-excluded-modes 'autoconf-mode)
   (global-aggressive-indent-mode 1))
 
 (use-package evil-surround
   :ensure t
   :after evil
+  :init
+  (defun evil-surround-region-for-hkkb (args)
+    (if (> 4 (length args))
+        args
+      (let* ((char (nth 3 args))
+             (new-char (cond
+                        ((= 33554474 char) 35) ;;<S-kp-multiply> => #
+                        ((= 33554479 char) 92) ;;<S-kp-divide> => \
+                        ((= 33554477 char) 95) ;;<S-kp-subtract> => _
+                        ((= 33554475 char) 61) ;;<S-kp-add> => =
+                        )))
+        (if new-char
+            (-replace-at 3 new-char args)
+          args))))
+
+  (defun evil-surround-chnage-for-hkkb (args)
+    (interactive (list (read-key)))
+    args)
+
   :config
+  (advice-add #'evil-surround-region :filter-args #'evil-surround-region-for-hkkb)
+  (advice-add #'evil-surround-change :filter-args #'evil-surround-chnage-for-hkkb)
+  (advice-add #'evil-surround-delete :filter-args #'evil-surround-chnage-for-hkkb)
   (global-evil-surround-mode 1))
 
 (use-package evil-multiedit
