c8eaed898a30ef86fe2539d3247ed6a6e6ab6ad6
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-05-24 20:35:23 +0900

    fix(emacs): update the font settings for Linux

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -1,8 +1,7 @@
 (when (window-system)
   (setq-default line-spacing 0)
-  (let* ((mac? (eq 'darwin system-type))
-         (font "MonacoB")
-         (height (if mac? 151 105)))
+  (let ((font "MonacoB")
+        (height (if (eq 'darwin system-type) 151 105)))
     (set-face-font 'default font)
     (set-face-attribute 'default nil
                         :weight 'bold
@@ -12,7 +11,10 @@
   (add-to-list 'face-font-rescale-alist '("Arial Unicode MS" . 0.95))
   (add-to-list 'face-font-rescale-alist '("Free-Symbola" . 0.95))
   (add-to-list 'face-font-rescale-alist '("Nanum Gothic" . 0.95))
-  (add-to-list 'face-font-rescale-alist '("STIXGeneral" . 0.9)))
+  (add-to-list 'face-font-rescale-alist '("STIXGeneral" . 0.9))
+  (when (eq 'gnu/linux system-type)
+    (add-to-list 'face-font-rescale-alist '("FontAwesome" . 0.95))
+    (add-to-list 'face-font-rescale-alist '("DejaVu Serif" . 0.95))))
 
 ;; hangul
 (set-language-environment "Korean")
