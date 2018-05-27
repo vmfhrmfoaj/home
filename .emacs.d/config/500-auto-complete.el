6d6c4444a51e1e1385e4172db3b6f777e0b31b88
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-05-27 23:37:34 +0900

    fix(emacs): disable company-dabbrev-downcase not to change suggestions when completing

--- .emacs.d/config/500-auto-complete.el
+++ .emacs.d/config/500-auto-complete.el
@@ -2,7 +2,8 @@
   :ensure t
   :config
   (setq company-idle-delay 0.2
-        company-selection-wrap-around t)
+        company-selection-wrap-around t
+        company-dabbrev-downcase nil)
   (global-company-mode 1))
 
 (use-package helm-company
