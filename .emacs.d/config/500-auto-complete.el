96968dad9c568ba2acf0eb91dd5c428fe47a7344
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-05-27 16:08:30 +0900

    fix(emacs): enable company-selection-wrap-around option

--- .emacs.d/config/500-auto-complete.el
+++ .emacs.d/config/500-auto-complete.el
@@ -1,7 +1,8 @@
 (use-package company
   :ensure t
   :config
-  (setq company-idle-delay 0.2)
+  (setq company-idle-delay 0.2
+        company-selection-wrap-around t)
   (global-company-mode 1))
 
 (use-package helm-company
