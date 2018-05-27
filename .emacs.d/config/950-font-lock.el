vmfhrmfoaj/home

commit fd293bce779df538ca74ae308d90c14f7280e1fb

fix(emacs/font-lock/clojure): update for highlighting macro local var(e.g. 'var-name#')

.emacs.d/config/950-font-lock.el
@@ -937,7 +937,7 @@
                     (goto-char local-limit))
                   (clojure-forward-sexp)
                   t)))
-             (concat meta? "\\_<" namespace? "\\(" symbol "\\)\\>"))
+             (concat meta? "\\_<" namespace? "\\(" symbol "\\)\\_>"))
            (save-excursion
              (if (in-comment?)
                  (setq font-lock--skip t)

commit c09883dd787f8d3be689533df2a1a41eed37f0e5

fix(emacs/clojure): fix for broken highlight rule (it is introduced from fd293bc)

.emacs.d/config/950-font-lock.el
@@ -647,7 +647,8 @@
                   "xml-seq"
                   "zero?"
                   "zipmap")
-                'symbols))
+                'symbols)
+    "TODO")
 
   (defcustom clojure--binding-forms
     '("binding" "doseq" "dotimes" "for" "let" "if-let" "if-some" "when-let" "when-some" "loop" "with-redefs")
@@ -658,9 +659,11 @@
                  (cl-every 'stringp value))))
 
   (defun clojure--binding-regexp ()
+    "TODO"
     (concat "(" (regexp-opt clojure--binding-forms) "[ \r\t\n]+\\["))
 
   (defun clojure-forward-sexp (&optional n)
+    "TODO"
     (or n (setq n 1))
     (while (not (zerop n))
       (if (> n 0) (clojure-skip  1 :comment :ignored-form :tagged-literal))
@@ -889,7 +892,7 @@
                     (goto-char local-limit))
                   (clojure-forward-sexp)
                   t)))
-             (concat meta? "\\_<" namespace? "\\(" symbol "\\)\\_>"))
+             (concat meta? "\\_<" namespace? "\\(" symbol "\\)"))
            (save-excursion
              (if (in-comment?)
                  (setq font-lock--skip t)