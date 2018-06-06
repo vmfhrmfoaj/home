aecc011c4ef239696e7352ac207c494bd81f3506
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-06-06 13:44:05 +0900

    fix(emacs): enable projectile chaching

--- .emacs.d/config/500-project.el
+++ .emacs.d/config/500-project.el
@@ -2,6 +2,7 @@
   :ensure t
   :config
   (setq projectile-completion-system 'helm
+        projectile-enable-cachig t
         projectile-mode-line "Ⓟ")
   (projectile-cleanup-known-projects)
   (projectile-mode 1))
