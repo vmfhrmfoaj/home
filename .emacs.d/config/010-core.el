8f75a293b09e6db8d18aec3cbec6d5f37555b056
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-06-11 22:55:50 +0900

    fix(emacs): remove evil-custom-forward-function

--- .emacs.d/config/010-core.el
+++ .emacs.d/config/010-core.el
@@ -1,24 +1,7 @@
 (use-package evil
   :ensure t
-  :init
-  (defvar evil-custom-forward-function nil
-    "TODO")
-
-  (defun evil-forward-symbol ()
-    "TODO"
-    (save-excursion
-      (let* ((fwd-sym (or evil-custom-forward-function #'forward-symbol))
-             (point (point))
-             (end   (progn (funcall fwd-sym  1) (point)))
-             (start (progn (funcall fwd-sym -1) (point))))
-        (if (and (not (= start point end))
-                 (<= start point end))
-            (cons start end)
-          (cons point (1+ point))))))
-
   :config
   (setq-default evil-symbol-word-search t)
-  (put 'evil-symbol 'bounds-of-thing-at-point #'evil-forward-symbol)
   (evil-mode 1))
 
 (use-package helm
