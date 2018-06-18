936e6d30f28e00cff91b7d712521fc22271bfe69
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-06-18 18:56:54 +0900

    fix(emacs): fix font settings for Linux

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -1,7 +1,7 @@
 (when (window-system)
   (setq-default line-spacing 0)
   (let ((font "MonacoB")
-        (height (if (eq 'darwin system-type) 141 95)))
+        (height (if (eq 'darwin system-type) 141 98)))
     (set-face-font 'default font)
     (set-face-attribute 'default nil
                         :weight 'bold
@@ -14,7 +14,6 @@
   (add-to-list 'face-font-rescale-alist '("STIXGeneral" . 0.9))
   (when (eq 'gnu/linux system-type)
     (add-to-list 'face-font-rescale-alist '("FontAwesome" . 0.95))
-    (add-to-list 'face-font-rescale-alist '("DejaVu Serif" . 0.95))
     (add-to-list 'face-font-rescale-alist '("all-the-icons" . 0.9))
     (add-to-list 'face-font-rescale-alist '("file-icons" . 0.9))))
 
