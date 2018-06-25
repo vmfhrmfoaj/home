c6396714f52c16e1ec6538981d24613a8780772f
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-06-25 19:22:11 +0900

    fix(emacs): decrease the font size on Linux

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -1,7 +1,7 @@
 (when (window-system)
   (setq-default line-spacing 0)
   (let ((font "MonacoB")
-        (height (if (eq 'darwin system-type) 141 98)))
+        (height (if (eq 'darwin system-type) 141 90)))
     (set-face-font 'default font)
     (set-face-attribute 'default nil
                         :weight 'bold
@@ -13,6 +13,7 @@
   (add-to-list 'face-font-rescale-alist '("Nanum Gothic" . 0.95))
   (add-to-list 'face-font-rescale-alist '("STIXGeneral" . 0.9))
   (when (eq 'gnu/linux system-type)
+    (add-to-list 'face-font-rescale-alist '("Fira Code Symbol". 0.95))
     (add-to-list 'face-font-rescale-alist '("FontAwesome" . 0.95))
     (add-to-list 'face-font-rescale-alist '("all-the-icons" . 0.9))
     (add-to-list 'face-font-rescale-alist '("file-icons" . 0.9))))
