292bb79fb8f99b039916e26295ca15f72bbd81e8
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-08-07 19:42:17 +0900

    fix(emacs): add settings for the Korean font

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -19,11 +19,13 @@
                         :weight 'medium
                         :width 'medium
                         :height height))
+  (set-fontset-font t 'hangul (font-spec :name "Noto Sans CJK KR"))
   (add-to-list 'face-font-rescale-alist '("Arial Unicode MS" . 0.9))
   (add-to-list 'face-font-rescale-alist '("DejaVu Serif"     . 0.9))
   (add-to-list 'face-font-rescale-alist '("FontAwesome"      . 0.85))
   (add-to-list 'face-font-rescale-alist '("Free-Symbola"     . 0.9))
   (add-to-list 'face-font-rescale-alist '("Material Icons"   . 0.9))
+  (add-to-list 'face-font-rescale-alist '("Noto Sans CJK KR" . 0.8))
   (add-to-list 'face-font-rescale-alist '("STIXGeneral"      . 0.9))
   (add-to-list 'face-font-rescale-alist '("Unifont"          . 0.95))
   (add-to-list 'face-font-rescale-alist '("Weather Icons"    . 0.9))
