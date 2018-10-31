9d9e265df1fe4bd8c691b811e53164b0065aebba
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2018-10-31 18:30:52 +0900

    fix(emacs): add a key bind for Emacs convention

--- .emacs.d/config/950-key-bind.el
+++ .emacs.d/config/950-key-bind.el
@@ -268,6 +268,7 @@
   :defer t
   :config
   (evil-global-set-key 'insert (kbd "C-h") #'backward-delete-char)
+  (evil-global-set-key 'insert (kbd "C-e") #'end-of-line)
   (evil-global-set-key 'visual (kbd "v") #'er/expand-region)
   (evil-global-set-key 'normal (kbd "TAB") #'aggressive-indent-do-indent)
   (evil-global-set-key 'visual (kbd "TAB") #'indent-region))
