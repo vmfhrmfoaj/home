742bc959fc48d3e138c4c4be2bd0262c73a10199
Jinseop Kim <vvmfhrmfoaj@yahoo.com>
2018-12-06 22:42:27 +0900

    fix(eamcs): fix for wiki settings

--- .emacs.d/config/200-wiki.el
+++ .emacs.d/config/200-wiki.el
@@ -1,5 +1,3 @@
 (use-package creole-mode
-  ;; :ensure t
-  :init
-  (unless (package-installed-p 'creole-mode)
-    (quelpa '(creole-mode :repo "nicferrier/creole-mode" :fetcher github))))
+  :ensure t
+  :defer t)
