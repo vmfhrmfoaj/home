bc000a8bb4a90d447676535b57cb29ccdcf9ab8b
Jinseop Kim <vvmfhrmfoaj@yahoo.com>
2018-12-26 18:25:34 +0900

    chore: update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,9 +3,8 @@
                   [lein-count  "1.0.7"]
                   [lein-pprint "1.1.2"]
                   [lein-project-version "0.1.0"]
-                  [lein-dev-helper  "0.2.0"]
-                  [lein-test-helper "0.1.3"]]
-        :middleware [lein-dev-helper.plugin/middleware]
+                  [my-lein-utils  "1.0.0"]]
+        :middleware [dev-helper.plugin/middleware]
         :repl-options {:init (do
                                (set! *print-level* 8)
                                (set! *print-length* 30))}}}
