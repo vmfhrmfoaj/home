73fdd548e23444b130eba31652ef2e8228f59015
Jinseop Kim <vvmfhrmfoaj@yahoo.com>
2018-12-27 22:27:52 +0900

    fix(emacs): update the color theme

--- .emacs.d/config/900-theme.el
+++ .emacs.d/config/900-theme.el
@@ -1,68 +1,37 @@
-(use-package zenburn-theme
+(use-package leuven-theme
   :ensure t
   :config
   (defvar local-variable-name-fg-color
-    (color-from 'font-lock-variable-name-face :foreground 0)
+    (saturate-color (color-from 'font-lock-variable-name-face :foreground 7) -20)
     "TODO")
   (setq x-underline-at-descent-line t)
-  (load-theme 'zenburn t)
   (custom-set-faces
    `(bold ((t (:weight bold))))
    `(cider-fringe-good-face ((t (:inherit success))))
-   `(clojure-define-type-face ((t (:inherit (bold font-lock-type-face)))))
-   `(clojure-defining-spec-face ((t (:inherit (bold font-lock-variable-name-face)))))
-   `(clojure-fn-parameter-face ((t (:foreground ,local-variable-name-fg-color :weight medium))))
-   `(clojure-keyword-face ((t (:inherit font-lock-builtin-face))))
+   `(clojure-define-type-face   ((t (:inherit (bold font-lock-type-face)))))
+   `(clojure-defining-spec-face ((t (:inherit (bold clojure-keyword-face)))))
+   `(clojure-fn-parameter-face  ((t (:foreground ,local-variable-name-fg-color :weight medium))))
+   `(clojure-keyword-face       ((t (:inherit font-lock-builtin-face))))
    `(clojure-local-binding-variable-name-face ((t (:inherit clojure-fn-parameter-face))))
-   `(clojure-side-effect-face ((t (:inherit (bold italic font-lock-warning-face)))))
+   `(clojure-side-effect-face   ((t (:inherit (bold italic font-lock-warning-face)))))
    `(clojure-special-variable-name-face ((t (:inherit font-lock-constant-face))))
-   `(font-lock-doc-face ((t (:slant italic))))
-   `(font-lock-function-name-face ((t (:weight bold))))
-   `(font-lock-variable-name-face ((t (:weight bold))))
-   `(helm-selection ((t (:weight bold :underline unspecified))))
-   `(lazy-highlight ((t (:background "#333333"))))
+   `(fringe ((t (:background "#FEFEFE"))))
+   `(hl-line ((t (:underline unspecified :inverse-video nil))))
+   `(hl-paren-face ((t (:weight bold))))
+   `(isearch ((t (:underline unspecified :weight bold))))
    `(lisp-local-binding-variable-name-face ((t (:foreground ,local-variable-name-fg-color :weight medium))))
-   `(org-done ((t (:underline t))))
-   `(org-todo ((t (:underline t))))
-   `(region ((t (:background "#DCDCCC" :foreground "#3F3F3F" :distant-foreground "#3F3F3F" :weight medium :underline nil :box nil :overline nil :slant normal :inverse-video nil)))))
-  (let ((diff-added-bg+5    (color-from 'diff-added   :background  5))
-        (diff-added-bg+15   (color-from 'diff-added   :background 15))
-        (diff-added-fg+5    (color-from 'diff-added   :foreground  5))
-        (diff-added-fg+15   (color-from 'diff-added   :foreground 15))
-        (diff-removed-bg+5  (color-from 'diff-removed :background  5))
-        (diff-removed-fg+5  (color-from 'diff-removed :foreground  5))
-        (diff-removed-bg+15 (color-from 'diff-removed :background 15))
-        (diff-removed-fg+15 (color-from 'diff-removed :foreground 15))
-        (diff-changed-bg+5  (color-from 'diff-changed :background  5))
-        (diff-changed-bg+15 (color-from 'diff-changed :background 15))
-        (diff-changed-fg+5  (color-from 'diff-changed :foreground  5))
-        (diff-changed-fg+15 (color-from 'diff-changed :foreground 15))
-        (selection-face '((t (:background "#5F5F5F" :distant-foreground "#DCDCCC")))))
-   (custom-theme-set-faces
-    'zenburn
-    `(diff-refine-added   ((t (:inherit diff-added   :background ,diff-added-bg+15   :foreground ,diff-added-fg+15))))
-    `(diff-refine-changed ((t (:inherit diff-removed :background ,diff-changed-bg+15 :foreground ,diff-changed-fg+15))))
-    `(diff-refine-removed ((t (:inherit diff-removed :background ,diff-removed-bg+15 :foreground ,diff-removed-fg+15))))
-    `(font-lock-builtin-face ((t (:foreground ,(saturate-color (color-from 'font-lock-keyword-face :foreground -5) -30)))))
-    `(evil-goggles-delete-face ((t (:inherit diff-refine-removed))))
-    `(evil-goggles-paste-face  ((t (:inherit diff-refine-added))))
-    `(evil-goggles-yank-face   ((t (:inherit diff-refine-changed))))
-    `(helm-match ((t (:inherit lazy-highlight))))
-    `(helm-match-selection ((t (:inherit isearch))))
-    `(helm-selection ,selection-face)
-    `(helm-swoop-target-line-face ((t (:inherit isearch))))
-    `(helm-swoop-target-word-face ((t (:inherit helm-match))))
-    `(isearch ((t (:foreground "#DCCFAB" :background "#1F1F1F"))))
-    `(italic ((t (:slant italic))))
-    `(magit-diff-file-heading-highlight ,selection-face)
-    `(magit-diff-added-highlight   ((t (:inherit diff-added   :background ,diff-added-bg+5   :foreground ,diff-added-fg+5))))
-    `(magit-diff-removed-highlight ((t (:inherit diff-removed :background ,diff-removed-bg+5 :foreground ,diff-removed-fg+5))))
-    `(magit-section-highlight ,selection-face)
-    `(org-scheduled-previously ((t (:inherit default))))
-    `(trailing-whitespace ((t (:underline "#CC9393"))))
-    `(whitespace-space ((t (:foreground "#5F5F5F")))))
-    `(whitespace-tab   ((t (:foreground "#5F5F5F"))))
-    `(whitespace-trailing ((t (:underline "#CC9393"))))))
+   `(org-date ((t (:underline unspecified :slant italic))))
+   `(org-link ((t (:inherit underline :underline unspecified)))))
+  (custom-theme-set-faces
+   'leuven
+   `(bold ((t (:weight bold))))
+   `(italic ((t (:slant italic))))
+   `(helm-swoop-target-word-face ((t (:inherit lazy-highlight))))
+   `(helm-match ((t (:inherit lazy-highlight))))
+   `(helm-match-selection ((t (:inherit isearch))))
+   `(helm-selection ((t (:background ,(color-from 'isearch :background 20) :distant-foreground "black" :weight medium))))
+   `(font-lock-doc-face ((t (:inherit font-lock-string-face :slant italic))))
+   `(font-lock-negation-char-face ((t (:inherit font-lock-warning-face :weight medium))))))
 
 (use-package elixir-mode
   :defer t
@@ -77,10 +46,10 @@
          (hbar-height (if (string-equal "gnome-imac" hostname)
                           (* 2 hbar-height) ; for HiDPI
                         hbar-height))
-         (default-color "#DCDCCC")
-         (visual-color "#000000")
-         (operator-color "#B967FF")
-         (replace-color "#B967FF"))
+         (default-color (color-from 'cursor :background   0))
+         (visual-color  (color-from 'cursor :background -15))
+         (operator-color "#A155E7")
+         (replace-color  "#DD9393"))
     (setq cursor-type 'box
           evil-normal-state-cursor   `(box ,default-color)
           evil-visual-state-cursor   `(hollow ,visual-color)
@@ -94,9 +63,9 @@
 (use-package highlight-parentheses
   :defer t
   :config
-  (setq hl-paren-base-color "Springgreen1"
+  (setq hl-paren-base-color "light sea green"
         hl-paren-colors
-        (--iterate (dim-color it 7)
+        (--iterate (dim-color it 6)
                    (apply 'color-rgb-to-hex-2-dig (color-name-to-rgb hl-paren-base-color))
                    4)))
 
@@ -119,7 +88,7 @@
   :after highlight-parentheses
   :config
   (custom-set-faces
-   `(show-paren-match    ((t (:foreground ,hl-paren-base-color :wiehgt bold))))
+   `(show-paren-match    ((t (:foreground ,hl-paren-base-color :weight bold :underline (:color "#D0372D")))))
    `(show-paren-mismatch ((t (:weight bold))))))
 
 (use-package php-mode
@@ -143,7 +112,7 @@
   :after highlight-parentheses
   :config
   (custom-set-faces
-   `(sp-show-pair-match-face    ((t (:foreground ,hl-paren-base-color :wiehgt bold))))
+   `(sp-show-pair-match-face    ((t (:foreground ,hl-paren-base-color :weight bold :underline (:color "#D0372D")))))
    `(sp-show-pair-mismatch-face ((t (:weight bold))))))
 
 (use-package web-mode
