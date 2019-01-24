266e1fa67f3eb6770cfbb80991f0663cd0d246ef
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2019-01-24 20:06:11 +0900

    fix(emacs): fix org-mode to search case insensitive tags

--- .emacs.d/config/200-org.el
+++ .emacs.d/config/200-org.el
@@ -30,6 +30,32 @@
       (org-deadline 'overwrite)
       nil))
 
+  (defun org-tags-completion-function-for-case-insensitive (string _predicate &optional flag)
+    "Complete tag STRING.
+FLAG specifies the type of completion operation to perform.  This
+function is passed as a collection function to `completing-read',
+which see."
+    (let ((completion-ignore-case t)	;tags are case-sensitive
+	        (confirm (lambda (x) (stringp (car x))))
+	        (prefix ""))
+      (when (string-match "^\\(.*[-+:&,|]\\)\\([^-+:&,|]*\\)$" string)
+        (setq prefix (match-string 1 string))
+        (setq string (match-string 2 string)))
+      (pcase flag
+        (`t (all-completions string org-last-tags-completion-table confirm))
+        (`lambda (assoc string org-last-tags-completion-table)) ;exact match?
+        (`nil
+         (pcase (try-completion string org-last-tags-completion-table confirm)
+	         ((and completion (pred stringp))
+	          (concat prefix
+		                completion
+		                (if (and org-add-colon-after-tag-completion
+			                       (assoc completion org-last-tags-completion-table))
+		                    ":"
+		                  "")))
+	         (completion completion)))
+        (_ nil))))
+
   (defun org-dic-at-point ()
     "TODO"
     (interactive)
@@ -66,6 +92,7 @@
             (lambda ()
               "TODO"
               (setq-local evil-lookup-func #'org-dic-at-point)))
+  (advice-add #'org-tags-completion-function :override #'org-tags-completion-function-for-case-insensitive)
   (advice-add #'org-todo :around
               (lambda (of &optional arg)
                 "If reopen the completed _TODO_, show a popup for logging."
