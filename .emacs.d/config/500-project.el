b15bd450161bf15240a4220e2f8721941cbd236c
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2019-01-26 18:38:10 +0900

    fix(emacs): fix `projectile-project-files` function to filter files ignored

--- .emacs.d/config/500-project.el
+++ .emacs.d/config/500-project.el
@@ -6,9 +6,24 @@
   :init
   (projectile-load-known-projects)
 
+  (defun projectile-project-files-custom-filter (files)
+    "TODO"
+    (let ((root (file-truename (projectile-project-root))))
+      (-if-let (regex (-some->> (append (projectile-project-ignored-files)
+                                        (-some->> (projectile-project-ignored-directories)
+                                                  (--map (concat it "/"))))
+                                (-map (-partial #'s-chop-prefix root))
+                                (regexp-opt)
+                                (concat "^")))
+          (-remove (-partial #'string-match-p regex) files)
+       files)))
+
   :config
-  (advice-add #'projectile-project-root :before-until (lambda (&optional _) (persp-current-project)))
+  (advice-add #'projectile-project-root :before-until
+              (lambda (&optional _) (persp-current-project)))
   (setq projectile-completion-system 'helm
         projectile-enable-cachig t)
+  (advice-add #'projectile-project-files :filter-return
+              #'projectile-project-files-custom-filter)
   (projectile-cleanup-known-projects)
   (projectile-mode 1))
