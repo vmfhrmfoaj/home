81df611d02dd6411d3088293e31419316aab03a6
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2019-01-28 19:04:44 +0900

    fix(emacs): disable customizing for HHKB

--- .emacs.d/config/000-env.el
+++ .emacs.d/config/000-env.el
@@ -1,4 +1,4 @@
 (let ((emacs-src-path (concat (getenv "HOME") "/Desktop/Open_Sources/emacs")))
-  (setq HHKB? t)
+  (setq HHKB? nil)
   (when (file-exists-p emacs-src-path)
     (setq source-directory emacs-src-path)))
