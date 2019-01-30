49844691cab3f64a880efb532beb566d66b5ec47
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2019-01-30 21:15:32 +0900

    fix(emacs): remove custom key binding: M-n/p

--- .emacs.d/config/950-key-bind.el
+++ .emacs.d/config/950-key-bind.el
@@ -282,10 +282,7 @@
   (evil-global-set-key 'normal (kbd "TAB") #'indent-for-tab-command)
   (with-eval-after-load 'aggressive-indent-mode
     (evil-global-set-key 'normal (kbd "TAB") #'aggressive-indent-do-indent))
-  (evil-global-set-key 'visual (kbd "TAB") #'indent-region)
-  (dolist (state '(normal visual))
-    (evil-global-set-key state (kbd "M-n") #'evil-forward-paragraph)
-    (evil-global-set-key state (kbd "M-p") #'evil-backward-paragraph)))
+  (evil-global-set-key 'visual (kbd "TAB") #'indent-region))
 
 (use-package evil-surround
   :defer t
--- .vimrc
+++ .vimrc
@@ -2,8 +2,3 @@ autocmd VimEnter * silent exec "! echo -ne '\e[1 q'"
 autocmd VimLeave * silent exec "! echo -ne '\e[6 q'"
 autocmd InsertEnter * silent exec "! echo -ne '\e[6 q'"
 autocmd InsertLeave * silent exec "! echo -ne '\e[1 q'"
-
-execute "set <M-n>=\en"
-execute "set <M-p>=\ep"
-map <M-n> }
-map <M-p> {
