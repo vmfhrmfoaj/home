fb54812e13e1361bf661efa68916625487f484e9
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2019-02-18 14:45:55 +0900

    revert(emacs): revert 46b4224

--- .emacs.d/config/500-version-control.el
+++ .emacs.d/config/500-version-control.el
@@ -41,14 +41,9 @@
   :defer t)
 
 (use-package magit
-  ;; NOTE
-  ;;  Use a custom version until fixing `evil-magit'.
-  ;; :ensure t
+  :ensure t
   :defer t
   :init
-  (unless (package-installed-p 'magit)
-    (quelpa '(magit :fetcher github :repo "vmfhrmfoaj/magit" :files ("lisp/*.el"))))
-
   (defface magit-commit-log-type-face
     `((t (:inherit font-lock-function-name-face :weight normal)))
     "TODO")
