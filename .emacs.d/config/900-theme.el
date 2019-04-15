f463f552b47ec8b9ef95347ead53ca663fdfe72d
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2019-04-15 12:00:02 +0900

    fix(emacs): update color theme

--- .emacs.d/config/900-theme.el
+++ .emacs.d/config/900-theme.el
@@ -7,20 +7,40 @@
         zenburn-scale-outline-headlines nil)
   :config
   (defvar local-variable-name-fg-color
-    (saturate-color (color-from 'font-lock-variable-name-face :foreground 7) -20)
+    (saturate-color (color-from 'font-lock-variable-name-face :foreground 5) -20)
     "TODO")
   (custom-set-faces
    `(helm-selection ((t (:distant-foreground ,(color-from 'default :foreground 0)))))
-   `(link ((t (:inherit underline :underline unspecified))))
+   `(link ((t (:inherit underline :underline unspecified :weight unspecified))))
    `(link-visited ((t (:inherit underline :underline unspecified))))
    `(lisp-local-binding-variable-name-face ((t (:foreground ,local-variable-name-fg-color :weight medium)))))
-  (custom-theme-set-faces
-   'zenburn
-   `(bold ((t (:weight ultrabold))))
-   `(italic ((t (:slant italic))))
-   `(isearch ((t (:background "#DFAF8F" :foreground "#2B2B2B" :weight bold))))
-   `(underline ((t (:underline (:color foreground-color :style wave)))))
-   `(variable-pitch ((t (:family "DejaVu Serif"))))))
+  (let ((diff-added-bg+5    (color-from 'diff-added   :background  5))
+        (diff-added-bg+15   (color-from 'diff-added   :background 15))
+        (diff-added-fg+5    (color-from 'diff-added   :foreground  5))
+        (diff-added-fg+15   (color-from 'diff-added   :foreground 15))
+        (diff-removed-bg+5  (color-from 'diff-removed :background  5))
+        (diff-removed-fg+5  (color-from 'diff-removed :foreground  5))
+        (diff-removed-bg+15 (color-from 'diff-removed :background 15))
+        (diff-removed-fg+15 (color-from 'diff-removed :foreground 15))
+        (diff-changed-bg+5  (color-from 'diff-changed :background  5))
+        (diff-changed-bg+15 (color-from 'diff-changed :background 15))
+        (diff-changed-fg+5  (color-from 'diff-changed :foreground  5))
+        (diff-changed-fg+15 (color-from 'diff-changed :foreground 15)))
+   (custom-theme-set-faces
+    'zenburn
+    `(bold ((t (:weight ultrabold))))
+    `(diff-refine-added   ((t (:inherit diff-added   :background ,diff-added-bg+15   :foreground ,diff-added-fg+15))))
+    `(diff-refine-changed ((t (:inherit diff-removed :background ,diff-changed-bg+15 :foreground ,diff-changed-fg+15))))
+    `(diff-refine-removed ((t (:inherit diff-removed :background ,diff-removed-bg+15 :foreground ,diff-removed-fg+15))))
+    `(evil-goggles-delete-face ((t (:inherit diff-refine-removed))))
+    `(evil-goggles-paste-face  ((t (:inherit diff-refine-added))))
+    `(evil-goggles-yank-face   ((t (:inherit diff-refine-changed))))
+    `(isearch ((t (:background "#DFAF8F" :foreground "#2B2B2B" :weight bold))))
+    `(italic ((t (:slant italic))))
+    `(magit-diff-added-highlight   ((t (:inherit diff-added   :background ,diff-added-bg+5   :foreground ,diff-added-fg+5))))
+    `(magit-diff-removed-highlight ((t (:inherit diff-removed :background ,diff-removed-bg+5 :foreground ,diff-removed-fg+5))))
+    `(underline ((t (:underline (:color foreground-color :style wave)))))
+    `(variable-pitch ((t (:family "DejaVu Serif")))))))
 
 (use-package auto-dim-other-buffers
   :defer t
@@ -105,7 +125,17 @@
    `(org-agenda-date ((t (:height 1.2))))
    `(org-agenda-date-today ((t (:inherit font-lock-function-name-face :foreground unspecified :height 1.2 :slant unspecified))))
    `(org-agenda-date-weekend ((t (:height 1.2))))
-   `(org-link ((t (:inherit link :underline unspecified))))))
+   `(org-block ((t (:inherit default :background "#323232"))))
+   `(org-block-begin-line ((t (:foreground "grey40" :background "#363636"))))
+   `(org-block-end-line   ((t (:foreground "grey40" :background "#363636"))))
+   `(org-done ((t (:foreground "grey70" :weight normal))))
+   `(org-link ((t (:inherit link :underline unspecified))))
+   `(org-verbatim ((t (:inherit variable-pitch)))))
+  (-when-let (strikethrough (assoc "+" org-emphasis-alist))
+    (setf (cdr strikethrough) '((:inherit shadow :strike-through t))))
+  (dolist (i (number-sequence 1 8))
+    (let ((face (intern (concat "org-level-" (number-to-string i)))))
+      (set-face-attribute face nil :inherit 'bold))))
 
 (use-package paren
   :after highlight-parentheses
