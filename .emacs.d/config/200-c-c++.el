7a2f0370758c007e85802f816744df879e837661
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2019-05-04 16:23:53 +0900

    fix(emacs): fix for C/C++

--- .emacs.d/config/200-c-c++.el
+++ .emacs.d/config/200-c-c++.el
@@ -10,9 +10,11 @@
   (setq c-default-style "linux")
   (add-hook 'c-mode-common-hook
             (lambda ()
+              (setq c-basic-offset 4)
               (setq-local evil-lookup-func #'man-at-point)
-              (c-toggle-auto-newline -1)
-              (setq c-basic-offset 4))
+              (make-local-variable 'font-lock-extend-region-functions)
+              (add-to-list 'font-lock-extend-region-functions #'font-lock-extend-region-wholelines)
+              (c-toggle-auto-newline -1))
             :append)
   (add-hook 'java-mode-hook
             (lambda ()
