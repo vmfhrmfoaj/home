6bd7b71a30989c8f7c6328b0ef9678d9965264cc
Jinseop Kim <jinseop.kim@antlabs.com>
2019-09-03 17:58:24 +0900

    fix(emacs): fix for Java

--- .emacs.d/config/200-c-c++.el
+++ .emacs.d/config/200-c-c++.el
@@ -10,10 +10,10 @@
   (setq c-default-style "linux")
   (add-hook 'c-mode-common-hook
             (lambda ()
+              (unless (and (fboundp #'editorconfig-core-get-nearest-editorconfig)
+                           (editorconfig-core-get-nearest-editorconfig default-directory))
+                (setq c-basic-offset 4))
               (when (member major-mode '(c-mode c++-mode))
-                (unless (and (fboundp #'editorconfig-core-get-nearest-editorconfig)
-                             (editorconfig-core-get-nearest-editorconfig default-directory))
-                  (setq c-basic-offset 4))
                 (setq-local evil-lookup-func #'man-at-point)
                 (make-local-variable 'font-lock-extend-region-functions)
                 (add-to-list 'font-lock-extend-region-functions #'font-lock-extend-region-wholelines)
