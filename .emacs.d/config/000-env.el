ee7869aa0674b5dd19b635cc236133097dce920e
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2019-10-11 22:17:48 +0900

    fix(emacs): remove custom 'source-directory'

--- .emacs.d/config/000-env.el
+++ .emacs.d/config/000-env.el
@@ -1,6 +1,2 @@
 (defvar home-dir (getenv "HOME"))
-
-(let ((emacs-src-path (concat home-dir "/Desktop/Open_Sources/emacs")))
-  (when (file-exists-p emacs-src-path)
-    (setq source-directory emacs-src-path)))
 (setq HHKB? t)
