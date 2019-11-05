e7d0bc85c96d522b8e9dec010710c80da232b83d
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2019-11-05 23:12:00 +0900

    fix(emacs): add missing color theme settings

--- .emacs.d/config/900-theme.el
+++ .emacs.d/config/900-theme.el
@@ -1,3 +1,7 @@
+(defvar local-variable-name-light-fg-color
+  (saturate-color (color-from 'font-lock-variable-name-face :foreground 7) -20)
+  "A foreground color for the local variable such as parameters")
+
 (custom-set-faces
  '(diff-added          ((((class color) (background light)) :background "#cceecc" :foreground "#22aa22")))
  '(diff-changed        ((((class color) (background light)) :background "#eeeecc" :foreground "#aaaa22")))
@@ -13,6 +17,19 @@
   (custom-set-faces
    '(auto-dim-other-buffers-face ((((class color) (background light)) :background "#F3F3F3")))))
 
+(use-package clojure-mode
+  :defer t
+  :config
+  (custom-set-faces
+   `(cider-fringe-good-face ((t :inherit success)))
+   `(clojure-define-type-face ((t :inherit (font-lock-type-face))))
+   `(clojure-defining-spec-face ((t :inherit (clojure-keyword-face))))
+   `(clojure-fn-parameter-face ((((class color) (background light)) :foreground ,local-variable-name-light-fg-color :weight medium)))
+   `(clojure-keyword-face ((t :inherit font-lock-builtin-face)))
+   `(clojure-local-binding-variable-name-face ((t :inherit clojure-fn-parameter-face)))
+   `(clojure-side-effect-face ((t :inherit (bold italic font-lock-warning-face))))
+   `(clojure-special-variable-name-face ((t :inherit font-lock-constant-face)))))
+
 (use-package helm
   :defer t
   :config
@@ -20,6 +37,17 @@
    '(helm-match ((t :inherit lazy-highlight)))
    '(helm-match-item ((t :inherit lazy-highlight)))
    '(helm-match-selection ((t :inherit isearch)))))
+
+(use-package php-mode
+  :defer t
+  :config
+  (custom-set-faces
+   `(php-passive-assign-variable-face ((t (:inherit font-lock-variable-name-face))))
+   `(php-variable-name ((((class color) (background light))
+                         (:foreground ,(-> 'font-lock-variable-name-face
+                                           (color-from :foreground 30)
+                                           (saturate-color 10)
+                                           (mix-color (color-from 'font-lock-string-face :foreground -30)))))))))
 
 (use-package powerline
   :defer t
@@ -30,6 +58,13 @@
                           :foreground "grey40"
                           :weight light
                           :box (:line-width -1 :color "grey75" :style nil))))))
+
+(use-package lsp-mode
+  :defer t
+  :config
+  (custom-set-faces
+   `(lsp-face-highlight-read  ((t (:inherit highlight :underline unspecified))))
+   `(lsp-face-highlight-write ((t (:inherit highlight :underline t))))))
 
 (use-package magit
   :defer t
