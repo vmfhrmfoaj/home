0422a4346b4a757ce04467ab0c0b2a0bccb428ea
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-04-01 10:43:07 +0900

    fix(emacs): change font and font size

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -1,19 +1,17 @@
 (when window-system
   (setq-default line-spacing 1)
   (set-face-font 'default
-                 (font-spec :family "Noto Sans Mono"
-                            :size 15)))
+                 (font-spec :family "Source Code Pro"
+                            :size 14)))
 
 (make-thread (lambda ()
                (prefer-coding-system 'utf-8)
-               (set-fontset-font t 'hangul "Noto Sans Mono CJK KR")
+               (set-fontset-font t 'hangul "Source Han Sans KR")
                (when window-system
                  (add-to-list 'face-font-rescale-alist '("Arial Unicode MS"   . 0.9))
                  (add-to-list 'face-font-rescale-alist '("DejaVu Sans"        . 0.9))
                  (add-to-list 'face-font-rescale-alist '("DejaVu Serif"       . 0.9))
-                 (add-to-list 'face-font-rescale-alist '("Free-Symbola"       . 0.85))
                  (add-to-list 'face-font-rescale-alist '("Noto Sans CJK"      . 0.9))
-                 (add-to-list 'face-font-rescale-alist '("Noto Sans Mono CJK" . 0.95))
                  (add-to-list 'face-font-rescale-alist '("STIXGeneral"        . 0.9))
-                 (add-to-list 'face-font-rescale-alist '("Source Han Sans"    . 0.85))
+                 (add-to-list 'face-font-rescale-alist '("Source Han Sans"    . 0.9))
                  (add-to-list 'face-font-rescale-alist '("Unifont"            . 0.95)))))
--- .emacs.d/config/900-window.el
+++ .emacs.d/config/900-window.el
@@ -1,5 +1,5 @@
 (when window-system
-  (let* ((min-col-width 150)
+  (let* ((min-col-width 170)
          (workarea (-some->> main-monitor (assoc 'workarea) (-drop 1)))
          (main-monitor-l (nth 0 workarea))
          (main-monitor-t (nth 1 workarea))
