2dfbae90ee1f47aea542cfc225bafbad74c25917
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-04-13 16:30:31 +0900

    fix(emacs): fix for wayland session

--- .emacs.d/config/000-env.el
+++ .emacs.d/config/000-env.el
@@ -1,2 +1,12 @@
 (defvar home-dir (getenv "HOME"))
+
 (setq HHKB? t)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (let* ((setup-file (concat home-dir "/.script/setup"))
+                   (cmd (concat "bash -c 'source " setup-file "; printenv PATH'")))
+              (when-let ((path (and (file-exists-p setup-file)
+                                    (string-trim (shell-command-to-string cmd)))))
+                (setenv "PATH" path)
+                (setq-default exec-path (split-string path ":"))))))
