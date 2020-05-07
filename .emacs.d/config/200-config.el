adcb2ae7af449fe4fe89bb84297b7ac9d5e8aa13
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-05-07 22:52:19 +0900

    fix(emacs): add `toml-mode`

--- .emacs.d/config/200-config.el
+++ .emacs.d/config/200-config.el
@@ -5,6 +5,10 @@
   (when (require 'highlight-numbers nil t)
     (puthash 'rpm-spec-mode "\\<[0-9]+\\>" highlight-numbers-modelist)))
 
+(use-package toml-mode
+  :ensure t
+  :defer t)
+
 (use-package yaml-mode
   :ensure t
   :defer t)
