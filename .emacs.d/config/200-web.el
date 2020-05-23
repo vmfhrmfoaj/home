5dd62a36c4bc1aadbeff631b46061d1c58a5bca9
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-05-23 15:06:37 +0900

    chroe(emacs): cleanup

--- .emacs.d/config/200-web.el
+++ .emacs.d/config/200-web.el
@@ -1,3 +1,8 @@
+(use-package css-mode
+  :defer t
+  :config
+  (setq css-indent-offset 2))
+
 (use-package js
   :defer t
   :config
@@ -24,6 +29,15 @@
               (with-eval-after-load "lsp-mode"
                 (setq-local evil-lookup-func #'lsp-describe-thing-at-point)))))
 
+(use-package mmm-mode
+  :defer t
+  ;; NOTE:
+  ;;  This package not included in the `MELPA'.
+  ;;:ensure t
+  :init
+  (unless (package-installed-p 'mmm-mode)
+    (quelpa '(mmm-mode :fetcher github :repo "purcell/mmm-mode"))))
+
 (use-package typescript-mode
   :ensure t
   :defer t
@@ -32,3 +46,24 @@
             (lambda ()
               (with-eval-after-load "lsp-mode"
                 (setq-local evil-lookup-func #'lsp-describe-thing-at-point)))))
+
+(use-package vue-mode
+  :ensure t
+  :defer t)
+
+(use-package web-mode
+  :ensure t
+  :defer t
+  :mode "\\.html\\.eex\\'"
+  :config
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2)
+  (add-to-list 'web-mode-engines-alist '("elixir" . "\\.eex\\'"))
+  (with-eval-after-load "smartparens"
+    (add-hook 'web-mode-hook
+              (lambda ()
+                (-update->> web-mode-auto-pairs
+                            (--map (let ((a (car it))
+                                        (b (substring (cdr it) 0 -1)))
+                                    `(,a . ,b))))))))
