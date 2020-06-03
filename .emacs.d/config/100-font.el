5042280260c6b30c75dce4904949d78c76aa3407
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-06-03 11:50:42 +0900

    fix(emacs): fix for unicode font

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -4,19 +4,15 @@
 (defvar alternative-font
   (font-spec :family "Source Code Pro" :size 14)
   "TODO")
-(defvar unicode-font
-  (font-spec :family "DejaVu Sans" :size 13)
-  "TODO")
 (defvar hangul-font
   (font-spec :family "Noto Sans CJK KR" :size 14)
   "TODO")
 
 (when window-system
   (prefer-coding-system 'utf-8)
-  (setq use-default-font-for-symbols nil)
   (setq-default line-spacing 1)
   (set-face-font 'default default-font)
   (set-fontset-font t '(#xE000 . #xF8FF) default-font) ; for ligature
   (set-fontset-font "fontset-default" '(#xE000 . #xF8FF) default-font) ; for ligature
-  (set-fontset-font "fontset-default" 'unicode unicode-font)
-  (set-fontset-font "fontset-default" 'hangul hangul-font))
+  (set-fontset-font "fontset-default" 'hangul hangul-font)
+  (add-to-list 'face-font-rescale-alist '("DejaVu Sans" . 0.9)))
