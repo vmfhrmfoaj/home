9b3e9d1fd1a32de19f2a868ea4898a660c81ebb2
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-06-19 17:07:46 +0900

    fix(emacs): fix for `rpm-spec-mode`

--- .emacs.d/config/200-config.el
+++ .emacs.d/config/200-config.el
@@ -1,6 +1,7 @@
 (use-package rpm-spec-mode
   :ensure t
   :defer t
+  :mode "\\.rpm\\'"
   :config
   (when (require 'highlight-numbers nil t)
     (puthash 'rpm-spec-mode "\\<[0-9]+\\>" highlight-numbers-modelist)))
