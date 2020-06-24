823a74dc93eb7f28e7cc29054e1f550dc23109a8
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-06-24 18:59:54 +0900

    chore(emacs): clean up

--- .emacs.d/config/200-php.el
+++ .emacs.d/config/200-php.el
@@ -3,66 +3,13 @@
   :defer t
   :mode ("\\.php\\'" . php-mode)
   :init
-  (defvar php-code-beg-re "<\\?php[ \t\r\n]")
-  (defvar php-code-end-re "[ \t\r\n]\\?>")
-
-  (defn php-goto-beg-of-php-code-block (&optional limit)
-    (while (and (< (point-min) (point))
-                (re-search-backward php-code-beg-re limit t)
-                (sp-point-in-string-or-comment))))
-
-  (defn php-goto-end-of-php-code-block (&optional limit)
-    (let ((cur (point)))
-      (while (and (< (point) (point-max))
-                  (re-search-forward php-code-end-re limit t)
-                  (sp-point-in-string-or-comment)))
-      (when (= cur (point))
-        (goto-char (point-max)))))
-
-  (defn php-code-regions ()
-    (let ((regions nil)
-          (beg (point-min))
-          (end (point-max))
-          min)
-      (save-excursion
-        (goto-char beg)
-        (while (not (= beg end))
-          (setq end (progn (php-goto-end-of-php-code-block)     (point))
-                beg (progn (php-goto-beg-of-php-code-block min) (point)))
-          (when (not (= beg end))
-            (add-to-list 'regions (cons beg end) t))
-          (setq min end)
-          (goto-char end))
-        regions)))
-
-  (defface php-unhilight-face
-    `((t (:inherit (shadow default) :weight ,(face-attribute 'default :weight))))
-    "TODO")
-
-  (defn php-unhilight-no-php-code ()
-    (let* ((php-code-regions (php-code-regions))
-           (non-php-code-regions
-            (->> (if (= (point-max) (cdr (-last-item php-code-regions)))
-                     php-code-regions
-                   (append php-code-regions (list (list (point-max)))))
-                 (--reduce-from
-                  (let ((beg (car it))
-                        (end (cdr it)))
-                    (append acc (list beg end)))
-                  nil)
-                 (-drop 1)
-                 (-partition 2))))
-      (dolist (region non-php-code-regions)
-        (let ((beg (car  region))
-              (end (cadr region)))
-          (overlay-put (make-overlay beg end) 'face 'php-unhilight-face)))))
-
   (add-hook 'php-mode-hook
             (lambda ()
               (with-eval-after-load "lsp-mode"
                 (setq-local evil-lookup-func #'lsp-describe-thing-at-point))
-              (make-local-variable 'font-lock-extend-region-functions)
-              (add-to-list 'font-lock-extend-region-functions #'font-lock-extend-region-wholelines))))
+              ;; (make-local-variable 'font-lock-extend-region-functions)
+              ;; (add-to-list 'font-lock-extend-region-functions #'font-lock-extend-region-wholelines)
+              )))
 
 (use-package psysh
   :disabled t
