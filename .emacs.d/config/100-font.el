be9fd74f17002583540733cff2758f0a2ba2d41a
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-07-13 21:27:56 +0900

    fix(emacs): use Noto CJK fonts

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -10,6 +10,10 @@
   (set-fontset-font "fontset-default" '(#xE000 . #xF8FF) default-font) ; for ligature
   (set-fontset-font "fontset-default" 'unicode (font-spec :family "Noto Color Emoji" :size 13))
   (set-fontset-font "fontset-default" 'unicode (font-spec :family "DejaVu Sans" :size 14) nil t)
-  (set-fontset-font "fontset-default" 'hangul  (font-spec :family "Noto Sans CJK KR" :size 14))
+  (set-fontset-font "fontset-default" 'hangul  (font-spec :family "Noto Sans CJK KR" :size 15))
+  (set-fontset-font "fontset-default" 'kana    (font-spec :family "Noto Sans CJK JP" :size 15))
+  (let ((font (font-spec :family "Noto Sans CJK SC" :size 15)))
+    (set-fontset-font "fontset-default" 'bopomofo font)
+    (set-fontset-font "fontset-default" 'han      font))
   (add-to-list 'face-font-rescale-alist '("DejaVu Sans Mono" . 0.95))
   (add-to-list 'face-font-rescale-alist '("Noto Sans Mono"   . 0.85)))
