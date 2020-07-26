1690fd6d38f3c40ae46b89c2d92d5a56b3d02454
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-07-27 00:13:32 +0900

    fix(emacs): fix for `evil`
    https://github.com/syl20bnr/spacemacs/pull/1376

--- .emacs.d/config/100-custom.el
+++ .emacs.d/config/100-custom.el
@@ -22,10 +22,16 @@
 (put 'narrow-to-region 'disabled nil)
 
 (blink-cursor-mode 1)
-(global-subword-mode 1)
 (global-auto-revert-mode 1)
 (prefer-coding-system 'utf-8)
 (recentf-mode 1)
+
+(define-category ?U "Uppercase")
+(define-category ?u "Lowercase")
+(modify-category-entry (cons ?A ?Z) ?U)
+(modify-category-entry (cons ?a ?z) ?u)
+(push '(?u . ?U) evil-cjk-word-separating-categories)
+(global-subword-mode 1)
 
 (when (eq system-type 'darwin)
   (setq mac-command-modifier 'meta
