19ef8ee072be5c8548e29f1fd701a209eef32173
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-08-19 01:40:19 +0900

    emacs: increase font size

--- .emacs.d/config/100-font.el
+++ .emacs.d/config/100-font.el
@@ -6,7 +6,7 @@
 (when window-system
   (prefer-coding-system 'utf-8)
   (setq-default line-spacing 0)
-  (let ((font (font-spec :family "Cascadia Code" :size 13)))
+  (let ((font (font-spec :family "Cascadia Code" :size 14)))
     (set-face-font 'default font)
     (set-fontset-font t '(#xE000 . #xF8FF) font)                  ; for ligature
     (set-fontset-font "fontset-default" '(#xE000 . #xF8FF) font)) ; for ligature
