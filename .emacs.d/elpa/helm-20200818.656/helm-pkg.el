70b19fe63796af32b32fb76417d905a1ce36e11e
vmfhrmfoaj <vmfhrmfoaj@yahoo.com>
2020-08-20 17:12:09 +0900

    emacs: daily package updating

--- .emacs.d/elpa/helm-20200818.656/helm-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "helm" "20200818.656" "Helm is an Emacs incremental and narrowing framework"
-  '((emacs "25.1")
-    (async "1.9.4")
-    (popup "0.5.3")
-    (helm-core "3.6.2"))
-  :commit "dc9d1be707e76fedadce1cb3a8a15bed6dac7fd0" :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
