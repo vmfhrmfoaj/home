cd07c296e15b9f2caead06191721a74c4e6d9ecb
vmfhrmfoaj <vmfhrmfoaj@gmail.com>
2020-09-17 00:57:30 +0900

    emacs: daily package update

---
+++ .emacs.d/elpa/dap-mode-20200915.1459/dap-mode-pkg.el
@@ -0,0 +1,20 @@
+(define-package "dap-mode" "20200915.1459" "Debug Adapter Protocol mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (lsp-mode "6.0")
+    (dash-functional "1.2.0")
+    (bui "1.1.0")
+    (f "0.20.0")
+    (s "1.12.0")
+    (lsp-treemacs "0.1")
+    (posframe "0.7.0"))
+  :commit "bfc30f671b8df5b71fbd54071a45d1b79a43f7b8" :keywords
+  '("languages" "debug")
+  :authors
+  '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
+  :maintainer
+  '("Ivan Yonchovski" . "yyoncho@gmail.com")
+  :url "https://github.com/yyoncho/dap-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
