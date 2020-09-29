11d016f7d7ecba51f81460a47b04ddedfce9b7b6
vmfhrmfoaj <vmfhrmfoaj@gmail.com>
2020-09-29 10:04:31 +0900

    emacs: remove ~/.emacs.d/elpa

--- .emacs.d/elpa/strace-mode-20171116.2039/strace-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/vmfhrmfoaj/.emacs.d/elpa/strace-mode-20171116.2039/strace-mode.el  -*- no-byte-compile: t -*-
-(define-package "strace-mode" "20171116.2039" "strace output syntax highlighting" 'nil :commit "2901baa968d5180ab985ac40ca22cc20914d01f5" :keywords '("languages") :authors '(("Preston Moore" . "prestonkmoore@gmail.com")) :maintainer '("Preston Moore" . "prestonkmoore@gmail.com"))
--- .emacs.d/elpa/treemacs-evil-20200716.2041/treemacs-evil-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/vmfhrmfoaj/.emacs.d/elpa/treemacs-evil-20200716.2041/treemacs-evil.el  -*- no-byte-compile: t -*-
-(define-package "treemacs-evil" "20200716.2041" "Evil mode integration for treemacs" '((emacs "25") (evil "1.2.12") (treemacs "0.0")) :commit "18baf5d412645ea780aec718f8960904d6bf91c1" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
