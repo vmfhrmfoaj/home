2ba3c5366010a22aea1d78b699cec89bbf850d10
vmfhrmfoaj <vmfhrmfoaj@gmail.com>
2020-10-08 10:26:56 +0900

    lein: update `my-lein-utils`

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-cljfmt "0.7.0"]
                   [lein-pprint "1.3.2"]
-                  [my-lein-utils  "1.0.2"]]
+                  [my-lein-utils  "1.0.3"]]
         :middleware [dev-helper.plugin/middleware]
         :repl-options {:init (do
                                (set! *print-level* 8)
