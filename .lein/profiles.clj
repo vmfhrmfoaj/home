6cf004d77dffef9fc0d7fc7a8d8abb850d9fffd8
Jinseop Kim <vmfhrmfoaj@gmail.com>
2021-03-13 21:11:29 +0900

    clojure: update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,10 @@
-{:repl {:jvm-opts ["-Xmx8g" "-XX:MaxGCPauseMillis=10" "-server"]},
- :user {:plugins [[lein-cljfmt "0.7.0"]
+{:repl {:jvm-opts ["-Xverify:none" "-XX:TieredStopAtLevel=4" "-XX:MaxGCPauseMillis=5" "-Xmx1g" "-server"]
+        :middleware [dev-helper.plugin/middleware]}
+ :user {:jvm-opts ["-Xverify:none" "-XX:TieredStopAtLevel=4" "-Xmx1g" "-server"]
+        :plugins [[lein-cljfmt "0.7.0"]
                   [lein-pprint "1.3.2"]
                   [my-lein-templates "1.0.1"]
                   [my-lein-utils  "1.0.4"]]
-        :middleware [dev-helper.plugin/middleware]
         :repl-options {:init (do
                                (set! *print-level* 8)
                                (set! *print-length* 30))}
