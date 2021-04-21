05a3add4ef37d7d704b9b2c1da7f349aa1675464
Jinseop Kim <vmfhrmfoaj@gmail.com>
2021-04-21 18:58:54 +0900

    update for Java/Graalvm

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,10 +2,9 @@
                       :timeout-in-sec 5
                       :notify-command ["notify-send" "--hint" "int:transient:1" "Test"]}}
  :repl {:auto-refresh {:notify-command ["notify-send" "--hint" "int:transient:1" "Reload"]}
-        :jvm-opts ["-Xverify:none" "-XX:TieredStopAtLevel=4" "-XX:MaxGCPauseMillis=5" "-Xmx2g" "-server"]
+        :jvm-opts ["-XX:MaxGCPauseMillis=5" "-Xmx2g" "-server"]
         :middleware [dev-helper.plugin/middleware]}
- :user {:jvm-opts ["-Xverify:none" "-XX:TieredStopAtLevel=4" "-XX:MaxGCPauseMillis=5" "-Xmx2g" "-server"]
-        :plugins [[lein-cljfmt "0.7.0"]
+ :user {:plugins [[lein-cljfmt "0.7.0"]
                   [lein-pprint "1.3.2"]
                   [my-lein-templates "1.0.1"]
                   [my-lein-utils  "1.0.4"]]
