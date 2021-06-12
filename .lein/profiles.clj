a55031259916a5239bdb88ce8a453e283b82ded2
Jinseop Kim <vmfhrmfoaj@gmail.com>
2021-06-12 22:31:54 +0900

    lein: update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,8 +6,8 @@
         :middleware [dev-helper.plugin/middleware]}
  :user {:plugins [[lein-cljfmt "0.7.0"]
                   [lein-pprint "1.3.2"]
-                  [my-lein-templates "1.0.1"]
-                  [my-lein-utils  "1.0.4"]]
+                  [my-lein-templates "1.0.2"]
+                  [my-lein-utils  "1.0.5"]]
         :repl-options {:init (do
                                (set! *print-level* 8)
                                (set! *print-length* 30))}}}
