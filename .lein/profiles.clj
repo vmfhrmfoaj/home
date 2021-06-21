6dd662d0ae9be47591c9187e8e6cd9e4375c64f3
Jinseop Kim <vmfhrmfoaj@gmail.com>
2021-06-22 01:59:31 +0900

    Setup Maven and Clojars local proxy

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,7 +4,12 @@
  :repl {:auto-refresh {:notify-command ["notify-send" "--hint" "int:transient:1" "Reload"]}
         :jvm-opts ["-XX:MaxGCPauseMillis=5" "-Xmx2g" "-server"]
         :middleware [dev-helper.plugin/middleware]}
- :user {:plugins [[lein-cljfmt "0.7.0"]
+ :user {:mirrors {"central" {:name "Maven (mirror)"
+                             :url "https://localhost:4430/repository/maven-central/"}
+                  "clojars" {:name "Clojars (mirror)"
+                             :url "https://localhost:4430/repository/clojars/"}}
+        :certificates ["/opt/local/localhost.crt"]
+        :plugins [[lein-cljfmt "0.7.0"]
                   [lein-pprint "1.3.2"]
                   [my-lein-templates "1.0.2"]
                   [my-lein-utils  "1.0.5"]]
