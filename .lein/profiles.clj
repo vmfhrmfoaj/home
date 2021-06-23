8a16a23465cd76d24f9627e29dc566c75c9a05eb
Jinseop Kim <vmfhrmfoaj@gmail.com>
2021-06-23 13:16:17 +0900

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,14 +5,17 @@
         :jvm-opts ["-XX:MaxGCPauseMillis=5" "-Xmx2g" "-server"]
         :middleware [dev-helper.plugin/middleware]}
  :user {:mirrors {"central" {:name "Maven (mirror)"
-                             :url "https://localhost:4430/repository/maven-central/"}
+                             :url "https://localhost:4430/repository/maven-public/"}
                   "clojars" {:name "Clojars (mirror)"
-                             :url "https://localhost:4430/repository/clojars/"}}
-        :certificates ["/opt/local/localhost.crt"]
+                             :url "https://localhost:4430/repository/clojars-group/"
+                             :repo-manager true}}
+        :certificates ["/opt/localhost/nexus.crt"]
+        :deploy-repositories [["snapshots" {:url "https://localhost:4430/repository/clojars-snapshots/" :sign-releases false}]
+                              ["releases"  {:url "https://localhost:4430/repository/clojars-releases/"  :sign-releases false}]]
         :plugins [[lein-cljfmt "0.7.0"]
                   [lein-pprint "1.3.2"]
-                  [my-lein-templates "1.0.2"]
-                  [my-lein-utils  "1.0.5"]]
+                  [vmfhrmfoaj/my-lein-templates "1.0.2"]
+                  [vmfhrmfoaj/my-lein-utils  "1.0.5"]]
         :repl-options {:init (do
                                (set! *print-level* 8)
                                (set! *print-length* 30))}}}
