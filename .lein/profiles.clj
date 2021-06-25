a096de7aa6538b0eb5bf23cc07ee3978e766b0d0
Jinseop Kim <vmfhrmfoaj@gmail.com>
2021-06-26 01:17:08 +0900

    Run Nexus repository manager on the home server

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,13 +5,13 @@
         :jvm-opts ["-XX:MaxGCPauseMillis=5" "-Xmx2g" "-server"]
         :middleware [dev-helper.plugin/middleware]}
  :user {:mirrors {"central" {:name "Maven (mirror)"
-                             :url "https://localhost:4430/repository/maven-public/"}
+                             :url "https://home.jinseop.kim:4430/repository/maven-public/"}
                   "clojars" {:name "Clojars (mirror)"
-                             :url "https://localhost:4430/repository/clojars-group/"
+                             :url "https://home.jinseop.kim:4430/repository/clojars-group/"
                              :repo-manager true}}
-        :certificates ["/opt/localhost/nexus.crt"]
-        :deploy-repositories [["snapshots" {:url "https://localhost:4430/repository/clojars-snapshots/" :sign-releases false}]
-                              ["releases"  {:url "https://localhost:4430/repository/clojars-releases/"  :sign-releases false}]]
+        :certificates ["/opt/public-keys/nexus.crt"]
+        :deploy-repositories [["snapshots" {:url "https://home.jinseop.kim:4430/repository/clojars-snapshots/" :sign-releases false}]
+                              ["releases"  {:url "https://home.jinseop.kim:4430/repository/clojars-releases/"  :sign-releases false}]]
         :plugins [[lein-cljfmt "0.7.0"]
                   [lein-pprint "1.3.2"]
                   [vmfhrmfoaj/my-lein-templates "1.0.3-SNAPSHOT"]
